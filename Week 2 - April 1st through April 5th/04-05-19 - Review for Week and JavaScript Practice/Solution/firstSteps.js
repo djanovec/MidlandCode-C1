f264eb114d6d3750d4b13bb2192bc4f57ce2b600
Mike Truax <mike@miketruax.com>
2019-04-05 14:33:55 -0500

    Updates simon says solution

--- Week 2 - April 1st through April 5th/04-05-19 - Review for Week and JavaScript Practice/Solution/firstSteps.js
+++ Week 2 - April 1st through April 5th/04-05-19 - Review for Week and JavaScript Practice/Solution/firstSteps.js
@@ -3,15 +3,15 @@ var buttons = document.getElementsByClassName('button');
 
 
 //Looping through the array and adding a simple click event to make sure they're wired up correctly.
-for(var i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener('click', function(){
-        console.log(e.target.id+" button clicked.")
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        console.log(this.id + " button clicked.")
     })
 }
 
 
 //Selecting a random element out of the buttons array and returning it. You could also return just a color
 //but selecting the element itself will make DOM manipulation much much easier.
-function getRandomButton(){
-    return buttons[Math.floor(Math.random()* buttons.length)];
+function getRandomButton() {
+    return buttons[Math.floor(Math.random() * buttons.length)];
 }
